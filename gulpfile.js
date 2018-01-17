5d72a4d8953f2434d7548b575e3b02f1d0d70b22
tg0825 <tg0825@backpack.kr>
2018-01-17 18:55:07 +0900

    init

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+// 패키지 변수
+const pkg = require('./package.json');
+
+// gulp
+const gulp = require('gulp');
+
+// devDependencies에 있는 모든 플러그인을 $ 변수에 로딩
+const $ = require('gulp-load-plugins')({
+    pattern: ['*'],
+    scope: ['devDependencies']
+});
+
+const onError = (err) => {
+    console.log(err);
+}
+
+const banner = [
+    "/**",
+    " * @project        <%= pkg.name %>",
+    " * @author         <%= pkg.author %>",
+    " * @build          " + $.moment().format("llll") + " ET",
+    " * @release        " + $.gitRevSync.long() + " [" + $.gitRevSync.branch() + "]",
+    " * @copyright      Copyright (c) " + $.moment().format("YYYY") + ", <%= pkg.copyright %>",
+    " *",
+    " */",
+    ""
+].join("\n");
+
+gulp.task('scss', () => {
+    $.fancyLog("-> Compiling scss");
+    return gulp.src(pkg.paths.src.scss + pkg.vars.scssName)
+        .pipe($.plumber({errorHandler: onError}))
+        .pipe($.sourcemaps.init({loadMaps: true}))
+        .pipe($.sass({
+            includePaths: pkg.paths.scss
+        }))
+        .pipe($.cached("sass_logError"))
+        .pipe($.autoprefixer())
+        .pipe($.sourcemaps.write('./'))
+        .pipe($.size({gzip: true, showFiles: true}))
+        .pipe(gulp.dest(pkg.paths.build.css));
+});
